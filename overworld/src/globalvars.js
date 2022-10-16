7261301a4729c642f0126fa19cc68f16ece326b0
jon <jon.halquist@gmail.com>
2022-10-16 16:09:21 -0500

    adjusting aspec ratio of background

--- overworld/src/globalvars.js
+++ overworld/src/globalvars.js
@@ -1,6 +1,8 @@
 const globalvars = {
-  'scene_height': window.innerHeight * .8,
-  'scene_width': window.innerWidth * .8,
+  // 'scene_height': window.innerHeight * .7,
+  // 'scene_width': window.innerWidth * .7,
+  'scene_height': 720,
+  'scene_width': 1280,
   'ctx': document.getElementById("root")
 }
 
