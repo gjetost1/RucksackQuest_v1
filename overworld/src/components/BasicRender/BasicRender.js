6e6245366653e37eb3b1baf56c096def0563fadc
jon <jon.halquist@gmail.com>
2022-10-16 21:52:32 -0500

    working on boundaries system

--- overworld/src/components/BasicRender/BasicRender.js
+++ overworld/src/components/BasicRender/BasicRender.js
@@ -1,9 +1,14 @@
 import React, { useContext, useEffect, useRef } from 'react'
 import './BasicRender.css'
+import black_square from '../../assets/sprites/black_square.png'
 import CanvasContext from '../CanvasContext'
 
 const height = 180
 const width = 320
+
+// move rate for character sprite
+let moveX = 1.2
+let moveY = 1.2
 
 const keys = {
   ArrowUp: {
@@ -80,15 +85,136 @@ const BasicRender = ({  }) => {
 
   useEffect(() => {
     const ctx = canvasRef.current.getContext('2d');
-    const rectSize = 16
-    const coordX = (width / 2) - (rectSize / 2)
-    const coordY = (height / 2) - (rectSize / 2)
-    ctx.fillRect(coordX, coordY, 16, 16)
-  },[])
+    const rectWidth = 16
+    const rectHeight = 16
+    const coordX = (width / 2) - (rectWidth / 2)
+    const coordY = (height / 2) - (rectHeight / 2)
+
+    class Sprite {
+      constructor({ image, position }) {
+        this.position = position
+        this.image = image
+      }
+
+      draw() {
+        ctx.drawImage(this.image, this.position.x, this.position.y, rectWidth, rectHeight)
+      }
+    }
+
+    const playerImage = new Image()
+    playerImage.src = black_square
+
+    const playerSprite = new Sprite({
+      image: playerImage,
+      position: {
+        x: coordX,
+        y: coordY
+      }
+    })
+
+    const checkBounds = (x, y) => {
+      if (x <= 0 && y <= 0) {
+        return {x: 0, y: 0}
+      }
+      if (x <= 0 && y >= height - rectHeight) {
+        return {x: 0, y: 0}
+      }
+      if (x >= width - rectWidth && y <= 0) {
+        return {x: 0, y: 0}
+      }
+      if (x >= width && y >= height - rectHeight) {
+        return {x: 0, y: 0}
+      }
+      if (x >= width - rectWidth) {
+        return {x: 0, y: 1.2}
+      }
+      if (x <= 0) {
+        return {x: 0, y: 1.2}
+      }
+      if (y >= height - rectHeight) {
+        return {x: 1.2, y: 0}
+      }
+      if (y <= 0) {
+        return {x: 1.2, y: 0}
+      }
+      else {
+        return {x: 1.2, y: 1.2}
+      }
+    }
+
+    // ctx.fillRect(coordX, coordY, 16, 16)
+
+    const animate = () => {
+      window.requestAnimationFrame(animate);
+      ctx.clearRect(0, 0, width, height);
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)'
+      ctx.fillRect(0, 0, width, height)
+
+      playerSprite.draw()
+      moveX = 1.2
+      moveY = 1.2
+      if (keys.ArrowDown.pressed && keys.ArrowRight.pressed) {
+        const {x, y} = checkBounds(playerSprite.position.x + 1, playerSprite.position.y + 1)
+        moveX = x
+        moveY = y
+        playerSprite.position.y = playerSprite.position.y + moveY;  // Move Down
+        playerSprite.position.x = playerSprite.position.x + moveX;  // Move Right
+      }
+      else if (keys.ArrowUp.pressed && keys.ArrowRight.pressed) {
+        const {x, y} = checkBounds(playerSprite.position.x - 1, playerSprite.position.y + 1)
+        moveX = x
+        moveY = y
+        playerSprite.position.y = playerSprite.position.y - moveY;  // Move Up
+        playerSprite.position.x = playerSprite.position.x + moveX;  // Move Right
+      }
+      else if (keys.ArrowDown.pressed && keys.ArrowLeft.pressed ) {
+        const {x, y} = checkBounds(playerSprite.position.x - 1, playerSprite.position.y - 1)
+        moveX = x
+        moveY = y
+        playerSprite.position.y = playerSprite.position.y + moveY;  // Move Down
+        playerSprite.position.x = playerSprite.position.x - moveX;  // Move Left
+      }
+      else if (keys.ArrowUp.pressed && keys.ArrowLeft.pressed ) {
+        const {x, y} = checkBounds(playerSprite.position.x - 1, playerSprite.position.y - 1)
+        moveX = x
+        moveY = y
+        playerSprite.position.y = playerSprite.position.y - moveY;  // Move Up
+        playerSprite.position.x = playerSprite.position.x - moveX;  // Move Left
+      }
+      else if (keys.ArrowDown.pressed) {
+        const {x, y} = checkBounds(width / 2, playerSprite.position.y + 1)
+        moveX = x
+        moveY = y
+        playerSprite.position.y = playerSprite.position.y + moveY;  // Move Down
+      }
+      else if (keys.ArrowUp.pressed) {
+        const {x, y} = checkBounds(width / 2, playerSprite.position.y - 1)
+        moveX = x
+        moveY = y
+        playerSprite.position.y = playerSprite.position.y - moveY;  // Move Up
+      }
+      else if (keys.ArrowRight.pressed) {
+        const {x, y} = checkBounds(playerSprite.position.x + 1, height / 2)
+        moveX = x
+        moveY = y
+        playerSprite.position.x = playerSprite.position.x + moveX;  // Move Right
+      }
+      else if (keys.ArrowLeft.pressed) {
+        const {x, y} = checkBounds(playerSprite.position.x - 1, height / 2)
+        moveX = x
+        moveY = y
+        playerSprite.position.x = playerSprite.position.x - moveX;  // Move Left
+      }
+
+    }
+    animate();
+  }, [])
 
   return (
-    <div id='canvas-container'>
-      <canvas ref={canvasRef} height={height} width={width} />
+    <div id='main-container'>
+      <div id='canvas-container'>
+        <canvas ref={canvasRef} height={height} width={width} />
+      </div>
     </div>
   )
 }
