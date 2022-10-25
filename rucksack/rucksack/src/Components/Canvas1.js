7907c8d7910ad400eb64153733b945bd39af30b8
gjetost1 <rosen659@umn.edu>
2022-10-25 17:40:23 -0500

    node_modules?

---
+++ rucksack/rucksack/src/Components/Canvas1.js
@@ -0,0 +1,14 @@
+
+import './Canvas1.css';
+
+function Canvas1() {
+
+
+  return (
+
+      <canvas id="tutorial" width="150" height="150" > </canvas>
+
+  );
+}
+
+export default Canvas1
---
+++ rucksack/src/components/droneRef.js
@@ -0,0 +1,23 @@
+import drone_up from '../assets/sprites/drone/drone_up.png'
+import drone_down from '../assets/sprites/drone/drone_down.png'
+import drone_left from '../assets/sprites/drone/drone_left.png'
+import drone_right from '../assets/sprites/drone/drone_right.png'
+import drone_upleft from '../assets/sprites/drone/drone_upleft.png'
+import drone_upright from '../assets/sprites/drone/drone_upright.png'
+import drone_downleft from '../assets/sprites/drone/drone_downleft.png'
+import drone_downright from '../assets/sprites/drone/drone_downright.png'
+
+
+
+const droneSprt = {
+  up: drone_up,
+  down: drone_down,
+  left: drone_left,
+  right: drone_right,
+  upleft: drone_upleft,
+  upright: drone_upright,
+  downleft: drone_downleft,
+  downright: drone_downright
+}
+
+export default droneSprt
